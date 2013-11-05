6c7378ef3b462af74aaf2b44a80abc47ce2f96d6
loicmartinez1 <loicmartinez1@fdb45d1f-df3e-4eff-1dfd-5eb9c71be8bd>
2013-11-05 14:53:45 +0000

    Commit DAO valide

--- GSB_medecins/src/java/modele/DAO.java
+++ GSB_medecins/src/java/modele/DAO.java
@@ -8,6 +8,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Collection;
 import java.util.HashSet;
 
 /**
@@ -16,61 +17,62 @@ import java.util.HashSet;
  */
 class DAO {
 
-     public static void charge(Brocante b) {
-       
+    public static Collection<Dep> getLesDeps() {
+        Collection<Dep> departements = new HashSet<Dep>();
+
         try {
             Connection con = Connect.get();
             Statement req;
             req = con.createStatement();
-            ResultSet rs = req.executeQuery( "select * from brocante");
-            rs.next();
-            b.setNom(rs.getString("nombroc"));
-            b.setLieu(rs.getString("lieubroc"));
-            b.setDate(rs.getDate("datebroc"));
-            Statement req2;
-            req2 = con.createStatement();
-            ResultSet rs2 =  req2.executeQuery("select * from typeemplacement order by prix");
-            while ( rs2.next() ) {
-                HashSet<Emplacement> e =new HashSet<Emplacement>();
-                Statement req3;
-                req3=con.createStatement();
-                ResultSet rs3= req3.executeQuery("select * from emplacement where idtype="+rs2.getString("idtype"));
-                while(rs3.next()){
-                    e.add(new Emplacement(rs3.getString("code"),rs3.getString("nomabonne")));
+            ResultSet rs = req.executeQuery("select distinct departement from medecin");
+
+            while (rs.next()) {
+
+                Collection<Med> medecins = new HashSet<Med>();
+                
+                Statement req2;
+                req2 = con.createStatement();
+                ResultSet rs2 = req2.executeQuery("select * from medecin where departement =" + rs.getString("departement"));
+                while (rs2.next()) {
+                    medecins.add(new Med(rs2.getString("nom") , rs2.getString("prenom"),rs2.getString("adresse"),rs2.getString("tel"),rs2.getString("specialitecomplementaire"),rs2.getString("id")));
                 }
-                
-                
-                
-                
-                
-                
-                
-                Type t = new Type(rs2.getString("idtype"),rs2.getString("description"),rs2.getDouble("prix"),e);
-                b.addType(t);
+                departements.add(new Dep(rs.getString("departement"), medecins));
+
+
+                /*
+                 * Connection con = Connect.get(); Statement req; req =
+                 * con.createStatement(); ResultSet rs = req.executeQuery(
+                 * "select * from brocante"); rs.next();
+                 * b.setNom(rs.getString("nombroc"));
+                 * b.setLieu(rs.getString("lieubroc"));
+                 * b.setDate(rs.getDate("datebroc")); Statement req2; req2 =
+                 * con.createStatement(); ResultSet rs2 =
+                 * req2.executeQuery("select * from typeemplacement order by
+                 * prix"); while ( rs2.next() ) { HashSet<Emplacement> e =new
+                 * HashSet<Emplacement>(); Statement req3;
+                 * req3=con.createStatement(); ResultSet rs3=
+                 * req3.executeQuery("select * from emplacement where
+                 * idtype="+rs2.getString("idtype")); while(rs3.next()){
+                 * e.add(new
+                 * Emplacement(rs3.getString("code"),rs3.getString("nomabonne")));
+                 * }
+                 *
+                 *
+                 *
+                 *
+                 *
+                 *
+                 *
+                 * Type t = new
+                 * Type(rs2.getString("idtype"),rs2.getString("description"),rs2.getDouble("prix"),e);
+                 * b.addType(t); } //Parcours rs.close();
+                 */
             }
-            //Parcours
-            rs.close();
-        
-        } catch(SQLException e) {
+        } catch (SQLException e) {
             System.out.println("Erreur SQL :" + e);
         }
-        
-    
+        return departements;
+
+
     }
-     public static void update(String code,String nomAbonne){
-     try {
-            Connection con = Connect.get();
-            Statement up;
-            up=con.createStatement();
-            up.executeUpdate("update emplacement set nomabonne='"+nomAbonne+"' where code='"+code+"'");
-     
-        
-        } catch(SQLException e) {
-            System.out.println("Erreur SQL :" + e);
-        }
-     
-         
-         
-     }
-    
 }
